fa3d74719980737da48d675c08de512785f5eb92
Evan Lee <evan.lee.615@gmail.com>
2024-03-23 21:27:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  let wordBackwards = ''
+  for (i = word.length - 1; i >= 0; i--) {
+    wordBackwards = wordBackwards + word[i]
+  }
+
+  if(word === wordBackwards) {
+    return true
+  } else {
+    return false
+  }
 }
+
+isPalindrome('test')
 
 /* 
   Add your pseudocode here
+
+  Need to loop through a word backwards
+
+  If that new word is strictly equal to the original word, then the original word is a palindrome
 */
 
 /*
   Add written explanation of your solution here
+
+  For a word like 'test', if we put that into line 15 and call our isPalindrome function, lines 3-6, or our backwards loop will set the variable wordBackwards to 'tset'
+
+  Then in lines 8-12, our if statement says that if 'test' is strictly equal to 'tset' then it will return true (test is a palindrome); if not, it will return false (test is not a palindrome); our if statement returns false
 */
 
-// You can run `node index.js` to view these console logs
+
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
